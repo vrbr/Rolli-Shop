466007272e53a7b44b03d4ec64d331e0c706bbe9
Мария <lpvamaria@gmail.com>
2021-07-24 11:46:33 +0300

    Показывает/скрывает блок Оформить заказ

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -2,9 +2,13 @@ function toggleCartStatus() {
     const cartWrapper = document.querySelector('.cart-wrapper');
     const cartEmptyBadge = document.querySelector('[data-cart-empty]');
 
+    const ofredForm = document.querySelector('#order-form');
+
     if (cartWrapper.children.length > 0) {
         cartEmptyBadge.classList.add('none');
+        ofredForm.classList.remove('none');
     } else {
         cartEmptyBadge.classList.remove('none');
+        ofredForm.classList.add('none');
     }
 }
